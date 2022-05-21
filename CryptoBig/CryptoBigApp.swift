a430f800901c419a71affcb3d7cec5585f1d0f31
Quang Bao <hoangquangbao.developer@gmail.com>
2022-05-21 16:01:55 +0700

    Initial CryptoBig project

---
+++ CryptoBig/CryptoBigApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBigApp.swift
+//  CryptoBig
+//
+//  Created by Quang Bao on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
