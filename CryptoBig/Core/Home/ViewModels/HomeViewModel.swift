1e39d7a797774dc9667b6c47203774e891464a20
Quang Bao <hoangquangbao.developer@gmail.com>
2022-07-03 21:47:03 +0700

    Transfer data to allCoins array

--- CryptoBig/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoBig/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,17 +6,30 @@
 //
 
 import SwiftUI
+import Combine
 
 class HomeViewModel: ObservableObject {
 
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    private var dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            self.allCoins.append(DeveloperPreview.instance.coin)
+//            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+//        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] returnCoins in
+                self?.allCoins = returnCoins
+            }
+            .store(in: &cancellables)
     }
 }
 
