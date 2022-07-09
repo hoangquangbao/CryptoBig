5bea59b7e7cd049dd91549ea8cf34e3d95bd72ef
Quang Bao <hoangquangbao.developer@gmail.com>
2022-07-09 22:00:13 +0700

    Deploy NetworkingManager

--- CryptoBig/Services/CoinDataService.swift
+++ CryptoBig/Services/CoinDataService.swift
@@ -1,3 +1,67 @@
+////
+////  CoinDataService.swift
+////  CryptoBig
+////
+////  Created by Quang Bao on 03/07/2022.
+////
+//
+//import SwiftUI
+//import Combine
+//
+//class CoinDataService {
+//
+//    @Published var allCoins: [CoinModel] = []
+//
+//    //1. Nếu triển khai tập cancel như này thì sẽ rất khó khi ta muốn hủy một đối tượng cụ thể nào đó
+////    var cancellables = Set<AnyCancellable>()
+//    //2. nên ta chuyển sang cách này
+//    var coinSubscription: AnyCancellable?
+//
+//    init() {
+//        getCoins()
+//    }
+//
+//    private func getCoins() {
+//
+//        //MARK: Step1: Download data from this url
+//        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+//        else { return }
+//
+//        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+//        //Step2: Using subscribe of combine to get data
+//            .subscribe(on: DispatchQueue.global(qos: .default))
+//        //Step3: Use map the url response
+//            .tryMap { output -> Data in
+//                //Strp3-1: Check it valid or not
+//                guard let response = output.response as? HTTPURLResponse,
+//                      response.statusCode >= 200 && response.statusCode < 300 else {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return output.data
+//            }
+//        //Step4: we received on the main thread
+//            .receive(on: DispatchQueue.main)
+//        //Step5: we decoded it into coin models
+//            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+//        //Step6: we handle the complition
+//            .sink { completion in
+//                switch completion {
+//                case .finished:
+//                    break
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//                //Step7: we got the return coins and we did something with it
+//            } receiveValue: { [weak self] returnedCoins in
+//                self?.allCoins = returnedCoins
+//                //3. Nếu ta ko có lệnh hủy ở đây thì cx ko vấn đề j bởi nó ko chiếm quá nhiều bộ nhớ
+//                //nhưng ta cx nên cancel nó đi bởi nó thật sự ko lm thêm bất cứ cái gì sau khi nó trả về data
+//                self?.coinSubscription?.cancel()
+//            }
+////            .store(in: &cancellables)
+//    }
+//}
+
 //
 //  CoinDataService.swift
 //  CryptoBig
@@ -27,37 +91,15 @@ class CoinDataService {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-        //Step2: Using subscribe of combine to get data
-            .subscribe(on: DispatchQueue.global(qos: .default))
-        //Step3: Use map the url response
-            .tryMap { output -> Data in
-                //Strp3-1: Check it valid or not
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-        //Step4: we received on the main thread
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
         //Step5: we decoded it into coin models
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
         //Step6: we handle the complition
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-                //Step7: we got the return coins and we did something with it
-            } receiveValue: { [weak self] returnedCoins in
-                self?.allCoins = returnedCoins
-                //3. Nếu ta ko có lệnh hủy ở đây thì cx ko vấn đề j bởi nó ko chiếm quá nhiều bộ nhớ
-                //nhưng ta cx nên cancel nó đi bởi nó thật sự ko lm thêm bất cứ cái gì sau khi nó trả về data
-                self?.coinSubscription?.cancel()
-            }
-//            .store(in: &cancellables)
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
+                    self?.allCoins = returnedCoins
+                    //3. Nếu ta ko có lệnh hủy ở đây thì cx ko vấn đề j bởi nó ko chiếm quá nhiều bộ nhớ
+                    //nhưng ta cx nên cancel nó đi bởi nó thật sự ko lm thêm bất cứ cái gì sau khi nó trả về data
+                    self?.coinSubscription?.cancel()
+            })
     }
 }
---
+++ CryptoBig/Utilities/NetworkingManager.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  CryptoBig
+//
+//  Created by Quang Bao on 09/07/2022.
+//
+/*
+ https://www.youtube.com/watch?v=pp5-ASYnY0o&list=PLwvDm4Vfkdphbc3bgy_LpLRQ9DDfFGcFu&index=9&ab_channel=SwiftfulThinking
+ */
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+//    static func download(url: URL) -> Publishers.ReceiveOn<Publishers.TryMap<Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>, Data>, DispatchQueue> {
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+        //Step2: Using subscribe of combine to get data
+            .subscribe(on: DispatchQueue.global(qos: .default))
+        //Step3: Use map the url response
+            .tryMap { output -> Data in
+                //Strp3-1: Check it valid or not
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+        //Step4: we received on the main thread
+            .receive(on: DispatchQueue.main)
+        
+        //MARK: As you can see return type above so crazy. Combine have a func convert return publisher type to return AnyPublisher type by use code line below
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
